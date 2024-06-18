425855a1ea369f3533e70cd1509bdf000aea4044
Filip Bengtegård <bengtegardbook@gmail.com>
2024-06-18 18:11:00 +0200

    can read operand atm

--- hw_1/sim8086.c
+++ hw_1/sim8086.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     FILE *fp;
-    unsigned char c;
+    unsigned char bytes[2];
+    char instructions[5];
 
     fp = fopen(argv[1], "rb");
     if (fp == NULL) 
@@ -11,14 +13,44 @@ int main(int argc, char *argv[])
         perror("Error opening file");
         return 1;
     }
-    printf("; %s disassembly:\n", argv[1]);
 
-    while(fread(&c, sizeof(char), 1, fp) > 0)
+    printf("; %s disassembly:\n", argv[1]);
+    printf("bits 16\n");
+
+    unsigned char operand = 0;
+    while(fread(bytes, sizeof(bytes), 1, fp) > 0)
     {
-        printf("%d\n", c);
+        int bitIndex = 0;
+        for (int i = 0;
+             i < 2; 
+             i++) 
+        {
+            for (int j = 7; 
+                 j >= 0; 
+                 j--) 
+            {
+                // Extract each bit from the byte, starting from the most significant bit
+                unsigned char bit = (bytes[i] >> j) & 1;
+                if (i == 0)
+                {
+                    operand |= bit << (5 - bitIndex);
+                }
+                bitIndex++;
+                printf("%u", bit); // Print the bit (for demonstration purposes)
+                // You can process the bit here as needed
+            }
+            if (operand == 34 && i == 0) 
+            {
+                strcat(instructions, "mov ");
+            }
+            printf("\n");
+        }
     }
 
-    // Close the file
+    printf("\n");
+    printf("Operand in number: %d\n", operand);
+    printf("%s", instructions);
+
     fclose(fp);
 
     return 0;
